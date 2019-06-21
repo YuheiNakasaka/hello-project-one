355f24b26951affe0110449ded48a71d3c1b55dc
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-06-21 23:08:51 +0900

    init

---
+++ components/Member.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="member">
+    <div class="member-name" :style="'background-color: ' + item.color">
+      <a :href="item.profile" target="_blank">{{ item.name }}</a>
+    </div>
+    <div class="member-sns">
+      <a
+        v-for="(sns, i) in item.sns"
+        :key="i"
+        :class="sns.class"
+        :href="sns.link"
+        >{{ sns.name }}</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Member',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  mounted() {
+    console.log(this.item)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.member {
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  .member-name {
+    flex-grow: 4;
+    a {
+      width: 100%;
+      padding: 11px 0;
+      text-align: center;
+      font-size: 18pt;
+      color: #fff;
+      font-weight: bold;
+    }
+  }
+  .member-sns {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    a {
+      font-size: 12pt;
+      padding: 5.5px 0;
+      color: #fff;
+      font-weight: bold;
+      &.instagram {
+        background-color: #833ab4;
+      }
+      &.ameba {
+        background-color: #2d8b3b;
+      }
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div class="groups">
+      <div class="group">
+        <div class="group-name">
+          <h1>モーニング娘。</h1>
+        </div>
+        <div class="members">
+          <Member
+            v-for="(member, i) in members"
+            :key="i"
+            :item="member"
+          ></Member>
+        </div>
+      </div>
+      <div class="group"></div>
+      <div class="group"></div>
+      <div class="group"></div>
+      <div class="group"></div>
+      <div class="group"></div>
+      <div class="group"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Member from '@/components/Member.vue'
+export default {
+  components: { Member },
+  data: function() {
+    return {
+      members: [
+        {
+          name: '譜久村聖',
+          yomi: 'ふくむらみずき',
+          color: '#e5007f',
+          profile:
+            'http://www.helloproject.com/morningmusume/profile/mizuki_fukumura/',
+          sns: [
+            {
+              name: 'Instagram',
+              class: 'instagram',
+              link: 'https://www.instagram.com/mizuki_fukumura.official/'
+            },
+            {
+              name: 'Ameba',
+              class: 'ameba',
+              link: 'https://ameblo.jp/morningmusume-9ki/theme-10059757620.html'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  .groups {
+    .group {
+      .group-name {
+        height: 50px;
+        background-color: rgb(47, 117, 188);
+        padding: 5px;
+        h1 {
+          margin: 0;
+          font-size: 20pt;
+          font-weight: bold;
+          color: #fff;
+        }
+      }
+      .members {
+      }
+    }
+  }
+}
+</style>
