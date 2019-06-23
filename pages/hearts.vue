dc3c410aa8220b6845e5b03140a7207f8db72498
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-06-23 12:43:06 +0900

    heartの空の時のやつ

--- pages/hearts.vue
+++ pages/hearts.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
     <Header :title="`お気に入り`"></Header>
-    <div class="groups">
+    <div v-if="allHearts.length > 0" class="groups">
       <div class="group">
         <div class="members">
           <Member v-for="(member, i) in allHearts" :key="`${i}_${member.color}`" :item="member"></Member>
         </div>
+      </div>
+    </div>
+    <div v-else class="no-items">
+      <div class="item">
+        <p>お気に入りがありません</p>
       </div>
     </div>
   </div>
@@ -54,6 +59,19 @@ export default {
       .members {
         width: 100%;
       }
+    }
+  }
+  .no-items {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 15px 30px;
+    color: #999999;
+    font-weight: bold;
+    text-align: center;
+    width: 300px;
+    .item {
     }
   }
 }
